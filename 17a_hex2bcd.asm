7f4fe21f3803fff70f6831b361121a8091b1306d
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-10-30 23:22:59 +0530

    17A changed

--- 17a_hex2bcd.asm
+++ 17a_hex2bcd.asm
@@ -1,23 +1,24 @@
 Print macro x
-	mov dx, offset x
-	mov ah, 09
-	int 21h
+    mov dx, offset x
+    mov ah, 09
+    int 21h
 endm
 
 get_input macro
-	mov ah, 01
-	int 21h
-	sub al, 30h
-	mov bl, al
-	int 21h
-	sub al, 30h
-	rol bl, 04
-	add al, bl
+    mov ah, 01
+    int 21h
+    sub al, 30h
+    mov bl, al
+    int 21h
+    sub al, 30h
+    rol bl, 04
+    add al, bl
 endm
 
 data segment
     n1 db ?
     msg1 db, 10, "Enter Hexadecimal Number: $"
+    msg2 db, 10, "Decimal Number: $"
     n2 db ?
 data ends
 
@@ -31,16 +32,31 @@ start:
 
         get_input
         mov n1, al
+        
+        Print msg2
 
         xor ax,ax
         mov al, n1
 
         aam           ;unpacks the number and stores it in ah and al.
-
+        
+        
+        mov bl, al    ; store al into bl
         mov cl, ah    ; store ah into cl.
-        rol cl, 04    ; rolled to left by 4 bits, for example 05 becomes 50.
 
-        add cl, al    ; added al and cl and stored in cl.
+        and cl, 0fh
+        add cl, 30h
+
+        mov dx,cx
+        mov ah,02
+        int 21h
+
+        and bl,0fh
+        add bl,30h
+
+        mov dx,bx
+        mov ah,02
+        int 21h
 
         
         mov ah,4ch
